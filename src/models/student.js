1b01bed324fc1e2ac2d856ca4902d844e5543d38
Lucas Olmedo Silva <lucas.silva@after.sale>
2024-12-06 13:48:43 -0300

    adds field and validation

--- src/models/student.js
+++ src/models/student.js
@@ -18,15 +18,44 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Student.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cpf: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 100],
+          msg: 'name must have between 3 and 100 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'invalid email',
+        },
+      }
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        isValidCpf: (field) => {
+          if (!/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/.test(field)) throw new Error('invalid cpf');
+        },
+      },
+    },
     enabled: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Student',
     tableName: 'students',
+    paranoid: true,
+    defaultScope: {
+      where: {
+        enabled: true
+      }
+    },
   });
   return Student;
 };
